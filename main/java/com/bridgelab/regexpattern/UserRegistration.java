468ed744cc478af11d06f86ee3dcb68dbfcdf9c7
AkshataHasabi <akshuh818@gmail.com>
2022-02-27 14:41:02 +0530

    ADDED:check lastname is valid or not

--- main/java/com/bridgelab/regexpattern/UserRegistration.java
+++ main/java/com/bridgelab/regexpattern/UserRegistration.java
@@ -5,11 +5,34 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static Scanner scanner=new Scanner(System.in);
+    static UserRegistration userRegistration = new UserRegistration();
     public static void main(String[] args) {
         System.out.println("welcome to regex programms");
-        UserRegistration userRegistration = new UserRegistration();
-        System.out.println("Enter a First Name:");
-        userRegistration.validFirstName(scanner.nextLine());
+        boolean condition = true;
+        while (condition) {
+            System.out.println("choose option:" + "\n" + "1.CheckFirstName" + "\n" + "2.CheckLastName" + "\n" + "3.Exit");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter a First Name:");
+                    userRegistration.validFirstName(scanner.next());
+                    break;
+                case 2:
+                    System.out.println("Enter a Last Name:");
+                    userRegistration.validLastName(scanner.next());
+                    break;
+                case 3:
+                    condition=false;
+                    break;
+                default:
+                    System.out.println("invalid option");
+            }
+        }
+    }
+
+    public void validLastName(String lastName) {
+        boolean value = Pattern.matches("^[A-Z]{1}[a-zA-Z]+", lastName);
+        System.out.println("LastName is valid or not:"+" "+ value);
     }
 
     public void validFirstName(String firstName) {
