41efe2d921d6c516e6763dcc5e9ca8f4c185a58f
AkshataHasabi <akshuh818@gmail.com>
2022-02-27 13:58:07 +0530

    ADDED:check firstname is valid or not

--- main/java/com/bridgelab/regexpattern/UserRegistration.java
+++ main/java/com/bridgelab/regexpattern/UserRegistration.java
@@ -1,7 +1,19 @@
 package com.bridgelab.regexpattern;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    static Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("welcome to regex programms");
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println("Enter a First Name:");
+        userRegistration.validFirstName(scanner.nextLine());
+    }
+
+    public void validFirstName(String firstName) {
+        boolean value = Pattern.matches("[A-Z]{1}[a-zA-Z]+", firstName);
+        System.out.println("Name is valid or not:"+ value);
     }
 }
